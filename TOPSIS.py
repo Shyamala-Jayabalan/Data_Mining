b24970949858906c1ab5f12bb40696fe66343dc9
shyamala-jayabalan <achuolu2007@gmail.com>
2019-05-12 16:38:25 -0400

    Update TOPSIS.py

--- TOPSIS.py
+++ TOPSIS.py
@@ -16,50 +16,56 @@ print("Running datetime:", dt.datetime.now())
 print(skcriteria.Data)
 # 2 alternatives by 3 criteria
 mtx = [
-[18,30,15,179,4,6,116,62,38,11],
-[31,69,40,346,14,3,113,39,61,20],
-[4,12,7,129,5,4,50,65,35,2],
-[12,39,29,201,7,5,97,48,52,12],
-[26,48,36,296,9,0,60,58,42,15],
-[25,58,32,298,12,3,122,53,57,17],
-[21,41,35,196,9,6,79,38,62,16],
-[10,25,18,198,5,8,185,55,45,9],
-[9,27,7,206,7,5,243,58,42,5]
+[25373,161,27.88,30110,84.26,28,142,94,2431,372,142,27410,694,39.49,5.72,41.3,16,4],
+[26500,264,41.21,29169,90.84,64,129,56,2485,522,174,25063,812,30.86,5.18,35.7,27,7],
+[25572,180,35.46,26833,95.3,51,136,68,2376,523,143,25704,705,36.45,5.62,38.8,29,9],
+[24993,185,37.98,26987,92.61,56,117,47,2353,446,181,23290,788,29.55,5.22,33.9,27,5],
+[24175,179,33.95,26579,90.95,42,133,67,2197,451,165,24537,743,33.02,5.43,36.4,20,10],
+[23742,237,34.76,26012,91.27,38,124,67,2192,494,165,23438,719,32.59,5.43,35.9,15,11],
+[21264,210,32.71,24938,85.26,33,120,67,1846,348,116,21443,570,37.61,5.29,42.6,18,9],
+[15080,215,26.69,17900,84.24,22,60,58,1194,497,81,17610,407,43.26,5.8,44.7,9,5],
+[14415,144,29.9,17600,81.9,23,80,53,1411,224,105,15052,455,33.08,5.09,38.9,16,5],
+[12451,138,22.31,16881,73.75,12,54,58,1145,191,94,15439,323,47.79,5.44,52.6,4,1],
+[4257,112,20.17,5671,75.06,3,18,27,408,66,29,6032,124,48.64,5.88,49.6,4,0]
 ]
 mtx
 
 # let's says the first two alternatives are
 # for maximization and the last one for minimization
-criteria = [MAX, MAX, MAX,MIN,MIN,MIN,MAX,MIN,MIN,MAX]
+criteria = [MAX, MAX,MAX,MAX,MAX,MAX,MAX,MIN,MAX,MAX,MAX,MIN,MAX,MIN,MIN,MIN,MAX,MAX]
 criteria
 
 # we use the built-in function as aliases
-data = Data(mtx, [max,max,max,min,min,min,max,min,min,max])
+data = Data(mtx, [max, max,max,max,max,max,max,min,max,max,max,min,max,min,min,min,max,max])
 
 
 
 data = Data(mtx, criteria,
-anames=["Pakistan", "India","Afghanistan","New zealand","England","South Africa","Australia","West Indies","Bangladesh"],
-cnames=["Centuries", "Fifties", "Chased","Total Wickets","Series Won","Series Loss","Catches Drop","Played on side","Played offside","Allout"])
+anames=["SriLanka", "India","England","South Africa","Australia","NewZealand","Pakistan","West Indies","Bangladesh","Zimbabwe","Ireland"],
+cnames=["Run", "HS","Average","BF","Strikerate","Centuries","Fifties","zeros","Fours","Sixes","Maiden","Runs_bowl","Wickets","Average_bowling","Economical","Strikerate_bowl","4wickets","5wickets"])
 
 
 data = Data(mtx, criteria,
-weights=[0.04, .3, .4,0.01,0.02,0.025,0.044,0.0234,0.1330,0.0046],
-anames=["Pakistan", "India","Afghanistan","New zealand","England","South Africa","Australia","West Indies","Bangladesh"],
-cnames=["Centuries", "Fifties", "Chased","Total Wickets","Series Won","Series Loss","Catches Drop","Played on side","Played offside","Allout"])
+#weights=[0.04, .3, .4,0.01,0.02,0.025,0.044,0.0234,0.1330,0.0046],
+weights=[0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556,0.0555555555555556],
+anames=["SriLanka", "India","England","South Africa","Australia","NewZealand","Pakistan","West Indies","Bangladesh","Zimbabwe","Ireland"],
+cnames=["Run", "HS","Average","BF","Strikerate","Centuries","Fifties","zeros","Fours","Sixes","Maiden","Runs_bowl","Wickets","Average_bowling","Economical","Strikerate_bowl","4wickets","5wickets"])
 
 
 
 
 #data.plot();
-#data.plot("box");
+data.plot("box");
+
+
+
 #data.plot.violin();
 #data.plot.radar(cmap="inferno", show_criteria=False);
 
 # first create the decision maker
 # (with the default hiper parameters)
-dm = simple.WeightedSum()
-
+dm = simple.WeightedSum()   
+print(dm)
  # Now lets decide the ranking
 dec = dm.decide(data)
 print(dec)
@@ -86,6 +92,11 @@ print(dec.best_alternative_, data.anames[dec.best_alternative_])
 # decision is not a beta-solution
 print(dec.kernel_)
 
+dm = simple.WeightedProduct()
+print(dm)
+
+dec = dm.decide(data)
+print(dec)
 
 #TOPSIS
 dm = closeness.TOPSIS()
@@ -103,15 +114,27 @@ print("Closeness:", dec.e_.closeness)
 
 #Finally we can change the normalization criteria of the alternative matric to sum (divide every value by the sum opf
 #their criteria) and check the result:
-dm = closeness.TOPSIS(mnorm="sum")
+#dm = closeness.TOPSIS(mnorm="sum")
+dm = closeness.TOPSIS()
 print(dm)
+
 
 print(dm.decide(data))
 
+dm = closeness.TOPSIS(mnorm="sum")
+dm
+print(dm)
+print(dm.decide(data))
+
+#The TOPSIS add more information into the decision object.
+print(dec.e_)
+print("Ideal:", dec.e_.ideal)
+print("Anti-Ideal:", dec.e_.anti_ideal)
+print("Closeness:", dec.e_.closeness)
 
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 ax1.set_title("Sum Norm")
 data.plot.violin(mnorm="sum", ax=ax1);
 ax2.set_title("Vector Norm")
 data.plot.violin(mnorm="vector", ax=ax2);
-f.set_figwidth(15)
+f.set_figwidth(50)
